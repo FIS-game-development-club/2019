844118f3799bd4a446d2ee330c754dfb05706b00
Mike B <mcb2022@gmail.com>
2018-05-29 16:22:08 +0200

    hi
    im michael

--- Assets/Scripts/MouseAppearance.cs
+++ Assets/Scripts/MouseAppearance.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class MouseAppearance : MonoBehaviour {
 
+
+	public Texture2D defaultMouse;
+	public CursorMode curMode = CursorMode.Auto;
+	public Vector2 hotspot = Vector2.zero;
+
 	// Use this for initialization
 	void Start () {
-		
+		Cursor.SetCursor(defaultMouse, hotspot, curMode);
 	}
 	
 	// Update is called once per frame
