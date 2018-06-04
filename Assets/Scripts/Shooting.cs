d48d9d8a6beac7a10cd171e200a5cde892ea3232
Mike B <mcb2022@gmail.com>
2018-06-04 13:36:55 +0200

    start
    Added branch, bullet texture, and shooting script (WIP).

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public Texture2D bullet;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1")){
+			Instantiate(bullet);
+		}
+	}
+}
