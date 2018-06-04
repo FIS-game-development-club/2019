4a973239bdde2d73288fdc0aa8172db5bc2bfbd3
Mike B <mcb2022@gmail.com>
2018-06-04 10:47:20 +0200

    commneted sutf

--- Assets/Scripts/MouseAppearance.cs
+++ Assets/Scripts/MouseAppearance.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class MouseAppearance : MonoBehaviour {
 
-
+	//put the mouse texture in this public variable in the editor
 	public Texture2D defaultMouse;
+	//really doesn't matter what there are, just defaults
 	public CursorMode curMode = CursorMode.Auto;
 	public Vector2 hotspot = Vector2.zero;
 
 	// Use this for initialization
 	void Start () {
+		//setting the cursor to our new mouse texture
 		Cursor.SetCursor(defaultMouse, hotspot, curMode);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
