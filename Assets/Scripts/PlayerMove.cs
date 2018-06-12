91d121e97ee36eae5ad8b32b6eb543ad0ea2db68
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-06-12 16:21:30 +0200

    working now

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -18,11 +18,14 @@ public class PlayerMove : MonoBehaviour {
 	void OnTriggerExit2D(Collider2D c){
 			collide = false;
 			jumptime = Time.time;
+			Debug.Log("not colliding");
 	}
 
 	void OnTriggerEnter2D(Collider2D c){
 			collide = true;
+			Debug.Log("colliding");
 	}
+
 
 	void FixedUpdate () {
 		if(Input.GetKey(KeyCode.D)){
