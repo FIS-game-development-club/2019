2c8baa82ec44fc076fee2b2f3f03ec56a945f942
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-08-21 16:37:39 +0200

    more edits

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -7,9 +7,11 @@ public class CameraMove : MonoBehaviour {
     private bool collide = false;
     private  int colliderY;
     private int colliderX;
+    private GameObject player;
+    public float speed;
 
     void Start (){
-        
+        player = GameObject.FindWithTag("player");
     }
 
     private void OnTriggerExit2D(Collider2D c)
@@ -29,7 +31,22 @@ public class CameraMove : MonoBehaviour {
 
     void Update () {
         if(collide){
-            
+            if (player.transform.position.x > transform.position.x)
+            {
+                transform.Translate(Vector2.right * ((float)(player.transform.position.x - transform.position.x - colliderX)) * speed);
+            }
+            if (player.transform.position.x < transform.position.x)
+            {
+                transform.Translate(Vector2.left * ((float)(transform.position.x - colliderX - player.transform.position.x)) * speed);
+            }
+            if (player.transform.position.y > transform.position.y)
+            {
+                transform.Translate(Vector2.up * ((float)(player.transform.position.y - transform.position.y - colliderY)) * speed);
+            }
+            if (player.transform.position.y < transform.position.y)
+            {
+                transform.Translate(Vector2.down * ((float)(transform.position.y - colliderY - player.transform.position.y)) * speed);
+            }
         }
 	}
 }
