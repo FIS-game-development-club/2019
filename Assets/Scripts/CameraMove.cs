539f28db3a61be5b7eb0559d16657c2d5d282277
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-08-21 15:24:08 +0200

    random shit

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	Collider col;
+	//create variable for collider
+
+	void Start () {
+		col = gameObject.GetComponent<
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
