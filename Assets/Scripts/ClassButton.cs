8959930a83390b471768bc777f305eff5049ad77
OctoMeep <roboneleven@gmail.com>
2018-09-11 16:50:25 +0200

    Finish button script, add class objects

--- Assets/Scripts/ClassButton.cs
+++ Assets/Scripts/ClassButton.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ClassButton : MonoBehaviour {
 
+	public string className;
+	public GameClass cls;
+	public ClassContainer classes;
+	public PlayerStats playerData; 
+
 	// Use this for initialization
 	void Start () {
-		
+		cls = classes.GetField(className)
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void TaskOnClick () {
+		playerData.cls = cls;
+		playerData.stats = cls.baseStats
 	}
 }
---
+++ Assets/Scripts/ClassContainer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassContainer : MonoBehaviour {
+
+	public GameClass rogue;
+	public GameClass warrior;
+	public GameClass ranger;
+	public GameClass mage;
+
+	// Use this for initialization
+	void Start () {
+		rogue = new GameClass("rogue", new Weapon[], new StatSet("Scripts/Classes/rogue.stat"));
+		warrior = new GameClass("warrior", new Weapon[], new StatSet("Scripts/Classes/warrior.stat"));
+		ranger = new GameClass("ranger", new Weapon[], new StatSet("Scripts/Classes/ranger.stat"));
+		mage = new GameClass("mage", new Weapon[], new StatSet("Scripts/Classes/mage.stat"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
