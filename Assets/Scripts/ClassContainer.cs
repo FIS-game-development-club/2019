15a9a3221f21029bc3a84bd012032063ad7e7242
OctoMeep <roboneleven@gmail.com>
2018-09-18 15:50:50 +0200

    Create stat files for classes, use lists for weapon lists

--- Assets/Scripts/ClassContainer.cs
+++ Assets/Scripts/ClassContainer.cs
@@ -11,10 +11,10 @@ public class ClassContainer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rogue = new GameClass("rogue", new Weapon[], new StatSet("Scripts/Classes/rogue.stat"));
-		warrior = new GameClass("warrior", new Weapon[], new StatSet("Scripts/Classes/warrior.stat"));
-		ranger = new GameClass("ranger", new Weapon[], new StatSet("Scripts/Classes/ranger.stat"));
-		mage = new GameClass("mage", new Weapon[], new StatSet("Scripts/Classes/mage.stat"));
+		rogue = new GameClass("rogue", new List<Weapon>(), new StatSet("Scripts/Classes/rogue.stat"));
+		warrior = new GameClass("warrior", new List<Weapon>(), new StatSet("Scripts/Classes/warrior.stat"));
+		ranger = new GameClass("ranger", new List<Weapon>(), new StatSet("Scripts/Classes/ranger.stat"));
+		mage = new GameClass("mage", new List<Weapon>(), new StatSet("Scripts/Classes/mage.stat"));
 	}
 	
 	// Update is called once per frame
