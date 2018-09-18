c21cb3a075130c3e7332c860154d4c4ea7475794
OctoMeep <roboneleven@gmail.com>
2018-09-18 16:03:32 +0200

    Fix class buttons

--- Assets/Scripts/GameClass.cs
+++ Assets/Scripts/GameClass.cs
@@ -1,10 +1,10 @@
 public class GameClass {
 
     public string name;
-    public Weapon[] allowedWeapons;
+    public List<Weapon> allowedWeapons;
     public StatSet baseStats;
 
-    GameClass (string _name, Weapon[] _allowedWeapons, StatSet _baseStats) {
+    GameClass (string _name, List<Weapon> _allowedWeapons, StatSet _baseStats) {
         name = _name;
         allowedWeapons = _allowedWeapons;
         baseStats = _baseStats;
